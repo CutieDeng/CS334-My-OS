07bb9b657af8729cfc2f038ac4870209888146aa
CutieDeng <CutieDeng@iCloud.com>
2022-07-30 23:07:04 +0800

     正确地输出相关的错误信息。

--- sheep_nucleus/src/panic.rs
+++ sheep_nucleus/src/panic.rs
@@ -1,6 +1,7 @@
 //! panic 库，实现 panic 和 abort 的基本功能。
 
-use core::fmt::{Write, Arguments};
+use core::fmt::Write; 
+use core::mem::MaybeUninit;
 
 use crate::panic::u8array::U8Array;
 /// 打印 panic 信息并 [`crate::shutdown`]. 
@@ -17,8 +18,18 @@ mod u8array {
     pub struct U8Array<'a> (pub &'a mut [u8], usize ); 
 
     impl <'a> U8Array<'a> {
+        /// 用一个 [`&mut [u8]`] 初始化 U8Array. 
+        /// 
+        /// 初始化后 U8Array 获得该切片的所有权。
         pub fn new(a: &'a mut [u8]) -> U8Array<'a> {
             Self (a, 0) 
+        }
+        pub fn clear(&mut self) {
+            self.1 = 0; 
+        }
+        pub fn as_str(&self) -> &str {
+            crate::println!("Invoke as str with size: {}", self.1); 
+            core::str::from_utf8(&self.0[..self.1]).unwrap()
         }
     }
 
@@ -47,36 +58,46 @@ fn panic_handler(info: &core::panic::PanicInfo) -> ! {
     // console::open_blue_print();
     let mut cached = [0u8; 1024]; 
     let mut cached = U8Array::new(&mut cached); 
-    for _ in 0..60 {
+    let mut tmp: [u8; 128] = unsafe { MaybeUninit::uninit().assume_init() }; 
+    let mut tmp = U8Array::new(&mut tmp); 
+    for _ in 0..20 {
         let _ = cached.write_str("\x1bD"); 
     }
     let _ = cached.write_str("\x1b[2J\x1b[1K"); 
-    let mid_print = |w: &mut U8Array, s: &str| {
-        for s in s.lines() {
-            let len = s.chars().map(|a| if a.is_ascii() {1} else if a.is_control() {0} else {2}).sum::<usize>(); 
-            if s.is_empty() {
-                let _ = write!(*w, "\r\n"); 
-                continue; 
-            }
-            if len > 40 {
-                continue; 
-            }
-            let sp = (80 - len) / 2; 
-            let _ = write!(*w, "\x1b[{}C{}\r\n", sp, s); 
+    let mid_print = |w: &mut U8Array, s: &str| -> bool { 
+        let len = s.chars().map(|a| if a.is_ascii() {1} else if a.is_control() {0} else {2}).sum::<usize>(); 
+        // println!("Read '{}', len = {}. ", s, s.len()); 
+        if s.is_empty() {
+            let _ = write!(w, "\r\n"); 
+            return true; 
         }
+        if len > 80 {
+            return false 
+        }
+        let sp = (80 - len) / 2; 
+        let _ = write!(w, "\x1b[{}C{}\r\n", sp, s); 
+        return true 
     }; 
-    mid_print(&mut cached, 
-        ":(\r\n你的电脑遇到问题，需要关机。\n我们只为您收集关键信息，然后为您关机。\n\n\n如果您需要了解更多信息，请查看此错误。\n"); 
-    // if let Some(location) = info.location() {
-    //     mid_print(&mut cached, &format_args!("{}:{} '{:?}'", location.file(), location.line(), {
-    //         info.message().unwrap_or(&format_args!("【无法解析错误信息】"))
-    //     })); 
-    // } else {
-    //     mid_print(&mut cached, &format_args!("{:?}", info.message()))
-    // }
-    let cached: &str = cached.into(); 
-    println!("{}", cached); 
-    println!("size of c: {}", cached.len()); 
+    let mid_print_lines = |w: &mut U8Array, s: &str, p: &dyn Fn(&mut U8Array, &str) -> bool| {
+        let mut succeed = true;
+        for s in s.lines() {
+            succeed = p(&mut *w, s) && succeed;
+        }
+        return succeed; 
+    }; 
+    mid_print(&mut cached, ":("); 
+    mid_print(&mut cached, "你的电脑遇到问题，需要关机。"); 
+    mid_print(&mut cached, "我们只为您收集关键信息，然后为您关机。"); 
+    mid_print(&mut cached, "如果您需要了解更多信息，请查看此错误。"); 
+    if let Some(location) = info.location() {
+        let _ = write!(tmp, "{}:{} {:?}", location.file(), location.line(), info.message().unwrap_or(&format_args!("无法解析的错误信息"))); 
+    } else {
+        let _ = write!(tmp, "{:?}", info.message().unwrap_or(&format_args!("无法解析的错误信息")));
+    }
+    mid_print_lines(&mut cached, tmp.as_str(), &mid_print); 
+    if false 
+    { tmp.clear(); } 
+    println!("{}", cached.as_str()); 
     shutdown(); 
 }
 
